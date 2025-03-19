cf09cff3bed95f735fd09ed18acbb16bdd2d2465
iwanharyatno <iwnharry61@gmail.com>
2025-03-19 19:59:16 +0700

    test .env reading

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log('Connecting to MongoDB', process.env.MONGO_URI);
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.error(err));
